4b78facece99ac470dbf01b6cb9ca367c9e862d1
yogagv <yoganandhan465@gmail.com>
2024-07-22 18:37:10 +0530

    tourbooking update

--- routes/tour.js
+++ routes/tour.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { allTour, createTour, deleteTour, getMaxGroupSize, getSingleTour, getTourByCity, getTourByDist, getTourCount, tourByFeatured, tourBySearch, updateTour } from '../controller/tourController.js';
+import { allTour, createTour, deleteTour, getMaxGroupSize, getSingleTour, getTourByCity, getTourByDist, tourByFeatured, tourBySearch, updateTour } from '../controller/tourController.js';
 import { authenticate, restrict } from '../auth/verifyToken.js';
 
 const router = express.Router();
@@ -14,7 +14,6 @@ router.get("/search/tourBySearch", tourBySearch)
 router.put("/updateTour/:id", updateTour);
 router.delete("/deleteTour/:id", authenticate, deleteTour);
 router.get("/search/tourByFeatured/:featured", tourByFeatured);
-router.get("/getTourCount", getTourCount);
 
 
 export default router;
